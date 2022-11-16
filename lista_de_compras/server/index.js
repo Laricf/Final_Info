f542c9b91caac4893ee10790c0d6535509ed17f3
“[firstname <[valid-email]>
2022-11-16 11:22:12 -0300

    inserindo server em lista_de_compras

---
+++ lista_de_compras/server/index.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(cors())
+app.use(express.json());
+
+const ControllerUsers = require("./controller/ControllerProduto");
+
+app.post('/produto/insert',      ControllerUsers.insert);
+app.delete('/produto/delete',    ControllerUsers.delete);
+app.get('produto/find',               ControllerUsers.find)
+
+
+
+app.post('/produto', (req, res) => {
+    let item = req.body.produto;
+    res.json(item);
+});
+
+app.post("/produto", (req, res) => {
+    const {produto} = req.body;
+    console.log(produto)
+
+    const response = "INSERT INTO mercado (produto) VALUES ?";
+
+    db.query(response, [produto], (err, result) => {
+        console.log("err");
+    })
+})
+
+
+
+
+
+app.listen(3001, () =>{
+    console.log("Rodando!!");
+});
