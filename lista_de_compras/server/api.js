eea5375b71846b2b9043bc48c7d6fe6b2d11f8df
“[firstname <[valid-email]>
2022-12-03 23:00:19 -0300

    limpando

--- lista_de_compras/server/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-    baseURL:'mysql://ufiwipddw1qhlebr:wOOoga7uXdHwt2N0GZMl@bnd5dssud6aytjfy9grh-mysql.services.clever-cloud.com:3306/bnd5dssud6aytjfy9grh'
-})
